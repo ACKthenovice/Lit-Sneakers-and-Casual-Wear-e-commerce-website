67c8fdd299c1a0200ee5e896dcf93d4929e25aa2
CharlesOkkamyo <okkamyo2001@gmail.com>
2023-09-23 12:23:53 +0630

    commit

--- pages/index.php
+++ pages/index.php
@@ -22,8 +22,6 @@
 
 </head>
 
-<body>
-    
     <?php
     include 'navigation.php';
     ?>
@@ -344,42 +342,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
     <script src="../js/script.js"></script>
 
-    <script>
-        var swiper = new Swiper(".mySwiper", {
-            slidesPerView: 1,
-            spaceBetween: 10,
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true,
-            },
-            autoplay: {
-                delay: 6000,
-                disableOnInteraction: false,
-            },
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
-            },
-            breakpoints: {
-                360: {
-                    slidesPerView: 1.5,
-                    spaceBetween: 20,
-                },
-                540: {
-                    slidesPerView: 2,
-                    spaceBetween: 20,
-                },
-                1200: {
-                    slidesPerView: 4,
-                    spaceBetween: 40,
-                },
-                1024: {
-                    slidesPerView: 3,
-                    spaceBetween: 20,
-                },
-            },
-        })
-    </script>
+ 
 
 </body>
 
