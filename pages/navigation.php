d6f55ea7deec9880e708aeee9679d75d3c666664
CharlesOkkamyo <okkamyo2001@gmail.com>
2023-09-25 22:49:56 +0630

    fixed navigation UI

--- pages/navigation.php
+++ pages/navigation.php
@@ -1,13 +1,10 @@
-<?php
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>navigation</title>
+    <title>Navigation</title>
 
     <!-- Add Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
@@ -15,22 +12,20 @@
     <link rel="stylesheet" href="../css/style.css">
     <!-- Add fontawesome -->
     <script src="https://kit.fontawesome.com/07c9fa584f.js" crossorigin="anonymous"></script>
-
-
 </head>
 
 <body>
 
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-white sticky-top">
-        <div class="container">
-            <a class="navbar-brand" href="index.php">
-                <img src="../images/Logo/litlogo-removebg-preview.png" alt="litlogo" width="45" height="24">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <a class="navbar-brand" href="index.php">
+            <img class="logo" src="../images/Logo/litlogo-removebg-preview.png" alt="litlogo">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
                         <a class="nav-link" href="index.php">Home</a>
@@ -43,20 +38,11 @@
                     </li>
                     <li class="nav-item">
                         <a href="cart.php" class="nav-link">
-                            <i class="fas fa-shopping-bag"></i>
+                            <i class="fa-solid fa-bag-shopping fa-bounce"></i>
                         </a>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
-
-
-
-
-
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
-    <script src="../js/script.js"></script>
 </body>
