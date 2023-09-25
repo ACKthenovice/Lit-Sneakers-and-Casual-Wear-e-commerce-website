726e9284ad2ed3d904c76ae839f94d8595b917da
CharlesOkkamyo <okkamyo2001@gmail.com>
2023-09-25 20:20:58 +0630

    admin page

---
+++ pages/admin.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel</title>
+    <link rel="stylesheet" href="admin-style.css">
+</head>
+<body>
+    <div class="sidebar">
+        <h1>Admin Panel</h1>
+        <ul>
+            <li><a href="#">Dashboard</a></li>
+            <li><a href="#">Users</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">Settings</a></li>
+        </ul>
+    </div>
+    <div class="content">
+        <header>
+            <h2>Welcome, Admin!</h2>
+            <a href="#">Log Out</a>
+        </header>
+        <main>
+            <h3>Dashboard</h3>
+            <p>This is the admin dashboard.</p>
+        </main>
+    </div>
+</body>
+</html>
