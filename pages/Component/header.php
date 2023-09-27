1300c36842cfa1187ed961cd9ec7737824d4f44a
CharlesOkkamyo <okkamyo2001@gmail.com>
2023-09-27 23:36:47 +0630

    admin dashboard template

--- pages/Component/header.php
+++ pages/Component/header.php
@@ -5,9 +5,17 @@
 <!-- Add custom CSS -->
 <link rel="stylesheet" href="../css/style.css">
 <!-- Add fontawesome -->
-<script src="https://kit.fontawesome.com/07c9fa584f.js" crossorigin="anonymous"></script>
+<!--<script src="https://kit.fontawesome.com/07c9fa584f.js" crossorigin="anonymous"></script>-->
+<link rel="stylesheet" data-purpose="Layout StyleSheet" title="Web Awesome" href="/css/app-wa-02670e9412103b5852dcbe140d278c49.css?vsn=d">
+<link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.4.2/css/all.css">
+<link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.4.2/css/sharp-solid.css">
+<link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.4.2/css/sharp-regular.css">
+<link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.4.2/css/sharp-light.css">
+<link rel="stylesheet" href="https://kit-pro.fontawsome.com/releases/v5.15.4/css/pro.min.css" >
 <!--Swiper Bundle for Slider-->
 <!--Swiper CSS-->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
 <!--Swiper JS-->
 <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+<!--Material design-->
+<link id="pagestyle" href="Admin/assets/css/material-dashboard.css?v=3.1.0" rel="stylesheet" />
