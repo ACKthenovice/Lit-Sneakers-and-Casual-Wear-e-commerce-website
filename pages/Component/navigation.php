5fe05844d9c95b7591c474dfae5d7aff4374bb3b
CharlesOkkamyo <okkamyo2001@gmail.com>
2023-09-28 12:50:00 +0630

    commit

--- pages/Component/navigation.php
+++ pages/Component/navigation.php
@@ -8,7 +8,7 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg blur top-0 z-index-3 shadow py-2 start-0 end-0 sticky-top">
         <div class="container-fluid ps-2 pe-0">
-            <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 " href="../pages/dashboard.html">
+            <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 " href="index.php">
                 L I T
             </a>
             <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
@@ -27,19 +27,19 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link me-2" href="../pages/profile.html">
+                        <a class="nav-link me-2" href="product.php">
                             <i class="fa-sharp fa-solid fa-box opacity-6 text-dark me-1"></i>
                             Products
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link me-2" href="../pages/sign-up.html">
+                        <a class="nav-link me-2" href="contact.php">
                             <i class="fa-solid fa-envelope opacity-6 text-dark me-1"></i>
                             Contact Us
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link me-2" href="../pages/sign-in.html">
+                        <a class="nav-link me-2" href="signin.php">
                             <i class="fa fa-user opacity-6 text-dark me-1"></i>
                             Sign In
                         </a>
